066838426030bbecca92ec9b5a404196cc2241fa
salmonte91 <stevenalmonte4@gmail.com>
2023-01-27 23:09:49 -0500

    adding and rendering new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,27 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-red-curry-image.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-image.jpg'
-      }]
-      
-    res.render('places/index', {places})
-})
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "/images/thai-red-curry-image.jpg",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "/images/coffee-image.jpg",
+    },
+  ];
 
-module.exports = router
+  res.render("places/index", { places });
+});
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+module.exports = router;
