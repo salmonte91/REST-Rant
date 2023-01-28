a29361620ac3a2eb8974ec3dc8b3eaca295a28f2
salmonte91 <stevenalmonte4@gmail.com>
2023-01-28 09:20:45 -0500

    form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -24,4 +24,10 @@ router.get("/", (req, res) => {
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
-app.use('/places', require('./controllers/places'))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/places", require("./controllers/places"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('*', (req, res) => {
-    res.render('error404')
-})
+app.get("*", (req, res) => {
+  res.render("error404");
+});
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
