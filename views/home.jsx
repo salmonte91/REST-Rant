5cb9ddee8bde439c49b15cc5bbc14585b3c4f06f
salmonte91 <stevenalmonte4@gmail.com>
2023-02-01 16:43:05 -0500

    Navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home() {
             <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src="/images/pizza-image.jpg" alt="Pizza Pie" />
+                    <img src="/images/pizza-image.jpg" alt="Pizza Pie" width = "750" height = "750" />
                     <div>
                     Photo by <a href="https://unsplash.com/ja/@saahilkhatkhate?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Saahil Khatkhate</a> on <a href="https://unsplash.com/images/food/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
